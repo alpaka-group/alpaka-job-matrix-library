2a696f4d1482636509e6d70a8bd593aad39ef95a
Simeon Ehrig <s.ehrig@hzdr.de>
2022-05-16 09:53:50 +0000

    Initial source code commit
    - The source code was developed during the renovation of the vikunja CI. Therefore, there is no real commit structure.
    - The code contains everything to generate the job combination matrix.
    - There are some rules implement, to avoid incorrect combination.
    - The example demonstrate, how the project specific part can be implemented.

---
+++ example/example_filter.py
@@ -0,0 +1,24 @@
+from alpaka_job_coverage.util import is_in_row
+
+from alpaka_job_coverage.globals import *  # pylint: disable=wildcard-import,unused-wildcard-import
+from example_globals import *  # pylint: disable=wildcard-import,unused-wildcard-import
+
+
+from packaging import version as pk_version
+
+
+def example_post_filter(row) -> bool:
+    if (
+        is_in_row(row, ALPAKA)
+        and is_in_row(row, BOOST)
+        and (
+            pk_version.parse(row[param_map[ALPAKA]][VERSION])
+            > pk_version.parse("0.8.0")
+            or row[param_map[ALPAKA]][VERSION] == "develop"
+        )
+        and pk_version.parse(row[param_map[BOOST]][VERSION])
+        < pk_version.parse("1.74.0")
+    ):
+        return False
+
+    return True
---
+++ example/example_globals.py
@@ -0,0 +1,4 @@
+"""This module contains constances used for the example job generation.
+"""
+
+ALPAKA: str = "alpaka"
